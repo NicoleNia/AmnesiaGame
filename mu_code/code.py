fb494d36ba81faac2c936ad1e1d2ac073a823ca7
PMCBlackstar <66456514+PMCBlackstar@users.noreply.github.com>
2023-05-01 10:28:16 -0400

    Update code.py

--- mu_code/code.py
+++ mu_code/code.py
@@ -1,4 +1,5 @@
 # Write your code here :-)
+# Write your code here :-)
 from adafruit_circuitplayground import cp
 import time
 
@@ -10,7 +11,8 @@ RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
 GREEN = (0, 255, 0)
-
+OFF = (0, 0, 0)
+score = 0 
 
 def turn_on_leds(color):
     for pixel_num in range(0, 4):
@@ -28,26 +30,35 @@ def turn_on_leds(color):
         time.sleep(0.2)
 
 
+def turn_off_all_leds(OFF):
+    for pixel_num in range(0, 10):
+        cp.pixels[pixel_num] = OFF
+        
+def level_1_seq():
+    for cp.pixels[pixel_num] = RED
+    
+    
 while True:
     if cp.button_a:
         print("Button A pressed!")
-        turn_on_leds(RED)
-        turn_on_leds(BLUE)
-        turn_on_leds(GREEN)
-        turn_on_leds(YELLOW)
+        turn_on_leds((RED, BLUE, GREEN, YELLOW))
+        time.sleep(1.0)
+        turn_off_all_leds(OFF)
+        global score = 0
 
     if cp.button_b:
         print("Button B pressed!")
-
+        level_1_seq()
     if cp.touch_A1:
         print("Touched pad A1")
-        turn_on_leds(RED)
-
     if cp.touch_A2:
         print("Touched pad A2")
-        turn_on_leds(BLUE)
+        
     if cp.touch_A3:
         print("Touched pad A3")
-        turn_on_leds(GREEN)
+        
     if cp.touch_A6:
         print("Touched pad A6")
+        turn_on_leds(YELLOW)
+    if cp.touch_A7:
+        print("Touched pad A7")
