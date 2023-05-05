9be15946ba036eeb9a24432eccf63bf877db85dc
PMCBlackstar <66456514+PMCBlackstar@users.noreply.github.com>
2023-05-05 10:31:02 -0400

    Update MLA GAME.py

--- mu_code/MLA GAME.py
+++ mu_code/MLA GAME.py
@@ -1,32 +1,32 @@
+# Write your code here :-)
 import pygame
 
 TITLE = "MLA"
-HEIGHT = 575
-WIDTH = 1000
+HEIGHT = 800
+WIDTH = 1500
 state = "start"
 gui_font = pygame.font.Font(None, 30)
 
 
 
 def draw():
-    screen.blit("mlsbackg.png", (0, 0))
+    screen.blit("background.jpg", (0, 0))
 
     if state == "start":
         screen.draw.text(
-            "Welcome to the Memory Loss Awareness Game!",
+            "FADED MEMORIES",
             color="white",
-            midtop=(WIDTH / 2, HEIGHT / 2 - 50),
+            midtop=(WIDTH / 3.8, HEIGHT / 1.85 - 30),
             owidth=1,
-            ocolor="blue",
-            fontsize=50,
+            ocolor="black",
+            fontsize=30,
         )
-
+        
         screen.draw.text(
             "Press Difficulty Level to Start",
             color="white",
-            midtop=(WIDTH / 2, HEIGHT / 2 + 50),
+            midtop=(WIDTH / 3.8, HEIGHT / 1.85 + 50),
             owidth=1,
-            ocolor="blue",
-            fontsize=30,
+            ocolor="black",
+            fontsize=20,
         )
-
