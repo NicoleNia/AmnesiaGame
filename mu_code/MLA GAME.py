1c27df5c347a8cb56ed10f77b906a7d2677d1b0c
PMCBlackstar <66456514+PMCBlackstar@users.noreply.github.com>
2023-05-30 10:09:41 -0400

    Update MLA GAME.py

--- mu_code/MLA GAME.py
+++ mu_code/MLA GAME.py
@@ -1,35 +1,12 @@
 # Write your code here :-)
 # Write your code here :-)
 import pygame
-import usb_hid
-from adafruit_circuitplayground.express import cpx
-from adafruit_hid.keyboard import Keyboard
-from adafruit_hid.keycode import Keycode
-from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
 
-kbd = Keyboard(usb_hid.devices)
-layout = KeyboardLayoutUS(kbd)
 TITLE = "MLA"
 HEIGHT = 800
 WIDTH = 1300
 state = "start"
 gui_font = pygame.font.Font(None, 30)
-
-while True:
-    if cpx.button_a:
-        kbd.send(Keycode.SHIFT, Keycode.A)  # Type capital 'A'
-        while cpx.button_a:  # Wait for button to be released
-            pass
-
-    if cpx.button_b:
-        kbd.send(Keycode.X)  # Type lowercase 'x'
-        while cpx.button_b:  # Wait for button to be released
-            pass
-    if cpx.touch_A3:
-        # Type 'Example Text' followed by Enter (a newline).
-        layout.write("Example Text\n")
-        while cpx.touch_A3:
-            pass
 
 
 def draw():
@@ -53,3 +30,4 @@ def draw():
             ocolor="black",
             fontsize=20,
         )
+       
